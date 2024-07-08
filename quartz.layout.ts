20a85d3ef88699d46d00b7f921c685bf368ab739
hex benjamin <hex@hexbenjam.in>
2024-07-08 12:44:58 -0400

    Quartz sync: Jul 8, 2024, 12:44 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -16,6 +16,12 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
+    Component.MobileOnly(Component.Explorer({
+      filterFn: (node) => {
+        const omit = new Set(["_assets"])
+        return !omit.has(node.name.toLowerCase())
+      },
+    })),
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
@@ -42,7 +48,12 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.MobileOnly(Component.Explorer({
+    filterFn: (node) => {
+      const omit = new Set(["_assets"])
+      return !omit.has(node.name.toLowerCase())
+    },
+  })), Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
